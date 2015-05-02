194618032d45518f70df5ddef98a3bb8bf6b7d8f
Ken Grønnbeck <kengroenn@gmail.com>
2015-05-02 18:20:05 +0200

    Included google maps

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,32 @@
 var React = require('react');
 
-var Measure = React.createClass({
-  render () {
-    return <h1>Hello World</h1>
+var ReactGoogleMaps = require('react-googlemaps');
+var GoogleMapsAPI = window.google.maps;
+
+var Map = ReactGoogleMaps.Map;
+var Marker = ReactGoogleMaps.Marker;
+var OverlayView = ReactGoogleMaps.OverlayView;
+
+class MeasureMap extends React.Component {
+  render() {
+    return (
+    <Map
+      initialZoom={10}
+      initialCenter={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}
+      width={700}
+      height={700}>
+
+      <Marker
+        position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)} />
+    </Map>)
   }
-});
+}
+
+class Measure extends React.Component {
+  render() {
+    return <MeasureMap/>
+  }
+};
 
 var domEl = document.getElementById('app');
 React.render(<Measure/>, domEl);
