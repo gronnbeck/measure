4b905c06478bc2ac6b171ec2c1ed9eea2826c09a
Ken Grønnbeck <kengroenn@gmail.com>
2015-05-02 18:47:51 +0200

    Display all my locations

--- src/app/index.js
+++ src/app/index.js
@@ -7,17 +7,50 @@ var Map = ReactGoogleMaps.Map;
 var Marker = ReactGoogleMaps.Marker;
 var OverlayView = ReactGoogleMaps.OverlayView;
 
+var request = require('superagent');
+
+class Coordinates {
+  constructor(lat, lng) {
+    this.lat = lat;
+    this.lng = lng;
+  }
+}
+
 class MeasureMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { locations: [] };
+  }
+
+  componentDidMount() {
+      request
+        .get('/locations')
+        .end((err, res) => {
+          if (err) { throw Error('Could not find locations endpoint') }
+          else {
+            var locations = JSON.parse(res.text);
+            this.setState({
+              locations: locations.map(
+                (l) => new Coordinates(l.latitude, l.longitude))
+            });
+          }
+        });
+  }
+
   render() {
+    const locations = this.state.locations;
+    const center = locations.length > 0 ?
+      locations[0] : new Coordinates(0, 0);
+    const markers = locations.map(
+      (l) => <Marker
+        position={new GoogleMapsAPI.LatLng(l.lat, l.lng)} />);
     return (
     <Map
-      initialZoom={10}
-      initialCenter={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}
+      initialZoom={3}
+      initialCenter={new GoogleMapsAPI.LatLng(center.lat, center.lng)}
       width={700}
       height={700}>
-
-      <Marker
-        position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)} />
+        {markers}
     </Map>)
   }
 }
